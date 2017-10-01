f9c944a3dd7db0643c9c0c8a724e88e076883fa9
Rob Lacey <contact@robl.me>
2017-10-01 17:38:42 +0100

    fix referrer

--- app/models/fossa/log.rb
+++ app/models/fossa/log.rb
@@ -1,5 +1,9 @@
 module Fossa
   class Log < ApplicationRecord
     serialize :ip, IpEncoder.new
+    before_save do
+      self.referer = self.referer.try(:[], 0..254)
+      self.path = self.path.try(:[], 0..254)
+    end
   end
 end
